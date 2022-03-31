d807e0c46f104c6e20156646ca90770c8dae654e
godeater <sagarbisht07x@gmail.com>
2022-03-31 22:43:06 +0530

    commit 4

--- app/src/main/java/com/fortysevenx/C/quest.kt
+++ app/src/main/java/com/fortysevenx/C/quest.kt
@@ -57,10 +57,27 @@ object quest{
             quesList.add(Questions(16,"To store 5.43234 which data type will you prefer?","Int","Double","Point","Float",1+12))
 
         //4
-            quesList.add(Questions(17,"Q1","op1","op2","op3","op4",2))
-            quesList.add(Questions(18,"Q2","op1","op2","op3","op4",2+4))
-            quesList.add(Questions(19,"Q3","op1","op2","op3","op4",2+8))
-            quesList.add(Questions(20,"Q4","op1","op2","op3","op4",2+12))
+        quesList.add(Questions(17,"Which of these is not a valid format specifier for data types?",
+            "%d",
+            "%s",
+            "%q",
+            "%c",
+            2))
+            quesList.add(Questions(18,"Can we print more than one types of data using only one printf statement?",
+                "Yes, C only allows us to print all type of data more than once inside the printf statement",
+                "No, C only allows us to print only same type of data more than once inside the printf statement",
+                "No we can only print only 1 data type and only once",
+                "It is possible but only inside the main() block",0+4))
+
+            quesList.add(Questions(19,"What is wrong with this input expression of an int x?\n\tScanf(\"%d\",x);\n",
+                "S is in uppercase, it should be 'scanf'",
+                "ampersand '&' is missing after x",
+                "ampersand '&' is missing before x","Both 1 and 3 are correct",3+8))
+
+            quesList.add(Questions(20,"Which of the following statement is incorrect?","\\n is used to start newline",
+                "a=>b is a correct expression for comparison between a and b in c",
+                "In C true is 1 and false is 0 or any negative number as boolean values.",
+                "a>=b is a correct expression for comparison between a and b in c",2+12))
 
 
 
